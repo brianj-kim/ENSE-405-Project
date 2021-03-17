47ffffc90692953ec798116798c551afe6726abf
brandon10003 <44481353+brandon10003@users.noreply.github.com>
2021-03-17 13:45:27 -0600

    Added firebase credentials

--- App/front-end/src/lib/firebase.js
+++ App/front-end/src/lib/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyB5-CfPptp47XDFjs_VSDzg11rqoYYiwGU',
-  authDomain: 'cloneinstagram-d1967.firebaseapp.com',
-  projectId: 'cloneinstagram-d1967',
-  storageBucket: 'cloneinstagram-d1967.appspot.com',
-  messagingSenderId: '795062103025',
-  appId: '1:795062103025:web:4f60fd9a6ac94baace7250'
+  apiKey: "AIzaSyDfaziErTVRgVf0qdLAYxsZEukP7twjSQU",
+  authDomain: "youniversity-243b5.firebaseapp.com",
+  projectId: "youniversity-243b5",
+  storageBucket: "youniversity-243b5.appspot.com",
+  messagingSenderId: "845063505361",
+  appId: "1:845063505361:web:06a38181d148bce0768cf0",
+  measurementId: "G-K6PQBTK7Q5"
 };
 
 const firebase = Firebase.initializeApp(config);
